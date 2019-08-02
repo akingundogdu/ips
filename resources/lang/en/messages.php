816a78dfc4cfa213306315dc75e479138ea39e60
Akin Gundogdu <akin-gundogdu@hotmail.com>
2019-08-02 13:42:16 +0300

    Added some error messages.

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,5 +3,6 @@
 return [
     'cesbn' => 'Customer email should not be null.',
     'cnfacfu' => 'Can not found any courses for the specified user.',
-    'mnfwc' => ':where module can not found for :course_key course.'
+    'mnfwc' => ':where module can not found for :course_key course.',
+    'fcnf' => 'First course can not found.'
 ];
