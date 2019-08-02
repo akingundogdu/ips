1ba2feea2b9b46ca701dec6876a333544c593d24
Akin Gundogdu <akin-gundogdu@hotmail.com>
2019-08-02 04:21:17 +0300

    Completed small helper function for code smell.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,22 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Response;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    function success(string $message, int $status)
+    /**
+     * @param string $message
+     * @param int $status
+     * @return JsonResponse
+     */
+    function success(string $message = '', int $status = 200)
     {
-
+        return $this->result($message, $status, true);
     }
 
-    function error(\Exception $exception)
+    /**
+     * @param Exception $e
+     * @return JsonResponse
+     */
+    function error(Exception $e)
     {
+        $status = 500;
+        if ($e->getCode() > 0) {
+            $status = $e->getCode();
+        }
+        return $this->result($e->getMessage(), $status, false);
+    }
 
+    /**
+     * @param string $message
+     * @param int $status
+     * @return JsonResponse
+     */
+    function errorWith(string $message, int $status = 500)
+    {
+        if (!$status)
+            $status = 500;
+        return $this->result($message, $status, false);
+    }
+
+    /**
+     * @param string $message
+     * @param int $status
+     * @param bool $is_succeeded
+     * @return JsonResponse
+     */
+    function result(string $message, int $status, bool $is_succeeded)
+    {
+        return Response::json(array('success' => $is_succeeded, 'message' => $message), $status);
     }
 }
