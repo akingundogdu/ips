6d524c411ef9c2d5d0a257c4fb3f33aadbb54211
Akin Gundogdu <akin-gundogdu@hotmail.com>
2019-08-02 04:19:30 +0300

    Added small helper function for code smell.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function success(string $message, int $status)
+    {
+
+    }
+
+    function error(\Exception $exception)
+    {
+
+    }
 }
