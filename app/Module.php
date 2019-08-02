5ec377c2c79a7f3a7fd56286f2aeb5478f6fb7e0
Akin Gundogdu <akin-gundogdu@hotmail.com>
2019-08-02 03:42:39 +0300

    Added users relation into Module model.

--- app/Module.php
+++ app/Module.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    //
+    public function users()
+    {
+        return $this->belongsToMany('App\User', 'user_completed_modules','module_id');
+    }
 }
