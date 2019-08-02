3267ca86a6c6d717ad98a1e4bcacd147d6e872ee
Akin Gundogdu <akin-gundogdu@hotmail.com>
2019-08-02 03:53:08 +0300

    Created httpClientHelper class.

---
+++ app/Http/Helpers/HttpClientHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Http\Helpers;
+
+
+class HttpClientHelper
+{
+    public function get(string $route_name, $parameters = array())
+    {
+
+    }
+
+    public function getWithUrl(string $uri, $parameters = array())
+    {
+
+    }
+}
